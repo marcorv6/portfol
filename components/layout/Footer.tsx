7a335885a657a8ad759cc2b5ef28289eec64c5ba
Marco Romero <yosoymarco3@gmail.com>
2025-11-22 18:58:16 -0600

    ✨ Add Header with navigation menu and Footer components

---
+++ components/layout/Footer.tsx
@@ -0,0 +1,25 @@
+import { Github, Instagram, Linkedin } from "lucide-react"
+import Link from "next/link"
+
+const Footer = () => {
+  return (
+    <div className="flex justify-between items-center p-4 border-t border-border dark:border-border/30 px-4 md:px-8 lg:px-12 xl:px-16 2xl:px-20 mx-auto">
+      <div className="flex flex-col items-center justify-center">
+        <p>&copy; 2025 Portfolio. All rights reserved.</p>
+      </div>
+      <div className="flex items-center justify-center gap-4">
+        <Link href="https://www.instagram.com/marco_antonio_cortes_garcia/" target="_blank">
+          <Instagram className="h-7 w-7 text-foreground dark:text-foreground/80" />
+        </Link>
+        <Link href="https://github.com/marco-antonio-cortes-garcia" target="_blank">
+          <Github className="h-7 w-7 text-foreground dark:text-foreground/80" />
+        </Link>
+        <Link href="https://www.linkedin.com/in/marco-antonio-cortes-garcia-b0b555200/" target="_blank">
+          <Linkedin className="h-7 w-7 text-foreground dark:text-foreground/80" />
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
