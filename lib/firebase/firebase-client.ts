5344a3c364d92f5e7530e856aa39e79061d4882c
Marco Romero <yosoymarco3@gmail.com>
2025-11-22 20:29:43 -0600

    Api key removed and rotated

--- lib/firebase/firebase-client.ts
+++ lib/firebase/firebase-client.ts
@@ -1,23 +1,16 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDCxwfiYpdXWUTXeqodIXmQ7hIt9rCkdSQ",
-  authDomain: "portfolio-cdf83.firebaseapp.com",
-  projectId: "portfolio-cdf83",
-  storageBucket: "portfolio-cdf83.firebasestorage.app",
-  messagingSenderId: "725571183054",
-  appId: "1:725571183054:web:d24805d781592fd65ccb9f",
-  measurementId: "G-Q97WYW1EEM"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-
 export default app;
